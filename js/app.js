f1c4db36a8f4677592da279f05939533a01edaf4
Jan Dahl-Madsen <jandahl-madsen@Jans-MacBook-Air.local>
2015-05-28 16:49:17 +0545

    Revert "add new enemy"
    This reverts commit 2960f81c3773962460131ed61b60997c3975e592.

--- js/app.js
+++ js/app.js
@@ -6,10 +6,6 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    //Placement
-    this.x = -98;
-    this.y = 310;
-    this.speed = 130;
 }
 
 // Update the enemy's position, required method for game
@@ -18,9 +14,6 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.speed *dt;
-    if(this.x > 503) this.x = -98;
-    if(this.x < -98) this.x = 503;
 }
 
 // Draw the enemy on the screen, required method for game
@@ -28,80 +21,15 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-var EnemyFast = function() {
-    //call prototype properties from Enemy
-    Enemy.call(this);
-
-    this.sprite = 'images/enemy-bug.png'
-
-    this.x = -98;
-    this.y = 227;
-    this.speed = 300;
-}
-// set EnemyFast prototype as a subclass of Enemy
-EnemyFast.prototype = Object.create(Enemy.prototype);
-// set EnemyFast constructor
-EnemyFast.prototype.constructor = EnemyFast;
-
-var EnemyBack = function() {
-
-    Enemy.call(this);
-    this.sprite = 'images/enemy-bug-back.png';
-
-    this.x = 503;
-    this.y = 144;
-    this.speed = -200;
-}
-// set EnemyBack prototype as a subclass of Enemy
-EnemyBack.prototype = Object.create(Enemy.prototype);
-// set EnemyBack constructor
-EnemyBack.prototype.constructor = EnemyBack;
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
-    this.sprite = 'images/char-princess-girl.png';
-    //placement
-    this.x = 100;
-    this.y = 475;
-}
-Player.prototype.update = function(dt) {
 
-}
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
-Player.prototype.handleInput = function(key) {
-    switch (key) {
-        case 'left':
-            if(this.x>0) this.x -= 100;
-            break;
-
-        case 'up':
-            if(this.y>0) this.y -= 83;
-            break;
-
-        case 'right':
-            if(this.x<350) this.x += 100;
-            break;
-
-        case 'down':
-            if(this.y<475) this.y += 83;
-            break;
-    }
-}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [
- new Enemy(),
- new EnemyFast(),
- new EnemyBack()
-];
 // Place the player object in a variable called player
-var player = new Player();
+
 
 
 // This listens for key presses and sends the keys to your
